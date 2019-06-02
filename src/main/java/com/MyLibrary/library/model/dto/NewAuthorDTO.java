2d355f013a0e023c327a436b2be3b06fedc4e18b
Patryk Kleczkowski <patryk.kleczkowski.96@gmail.com>
2019-06-02 20:30:15 +0200

    Backend - fix authorDTO class

--- src/main/java/com/MyLibrary/library/model/dto/NewAuthorDTO.java
+++ src/main/java/com/MyLibrary/library/model/dto/NewAuthorDTO.java
@@ -2,9 +2,11 @@ package com.MyLibrary.library.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class NewAuthorDTO {
 
     private String firstName;
